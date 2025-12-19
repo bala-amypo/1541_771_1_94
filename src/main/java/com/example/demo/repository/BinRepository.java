27fefced4dc761124667bcc4e81143be6de2c778
bala-amypo <bala@amypo.in>
2025-12-19 09:24:15 +0000

    User commit

--- src/main/java/com/example/demo/repository/BinRepository.java
+++ src/main/java/com/example/demo/repository/BinRepository.java
@@ -1,1 +1,14 @@
+package com.example.demo.repository;
 
+// import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Bin;
+
+public interface BinRepository extends JpaRepository<Bin,Long> {
+
+    // boolean existsByIdentifier(String identifier);
+
+    // List<Bin> findByActiveTrue();
+}
