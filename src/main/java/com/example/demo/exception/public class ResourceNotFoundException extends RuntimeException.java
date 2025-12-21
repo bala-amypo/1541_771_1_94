3174c62317a1e1f95bb3432226dd4aa9857e8e4a
bala-amypo <bala@amypo.in>
2025-12-21 07:32:55 +0000

    User commit

--- src/main/java/com/example/demo/exception/public class ResourceNotFoundException extends RuntimeException.java
+++ src/main/java/com/example/demo/exception/public class ResourceNotFoundException extends RuntimeException.java
@@ -1,8 +1,5 @@
 package com.example.demo.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
--- src/main/java/com/example/demo/service/impl/ZoneServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ZoneServiceImpl.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-// import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Zone;
 import com.example.demo.repository.ZoneRepository;
 import com.example.demo.service.ZoneService;
