8c1ae7bf5a7ce19fe67a12525f24cea8540a103d
bala-amypo <bala@amypo.in>
2025-12-27 04:25:40 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
     private final UserService userService;
@@ -31,16 +31,12 @@ public class AuthController {
         return ResponseEntity.ok(new ApiResponse(true, "User registered", saved));
     }
 
-    // ---- LOGIN (TOKEN) ----
-    @PostMapping("/token")
+    @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-
         User user = userService.getByEmail(request.getEmail());
 
         UsernamePasswordAuthenticationToken auth =
-                new UsernamePasswordAuthenticationToken(
-                        user.getEmail(), request.getPassword()
-                );
+                new UsernamePasswordAuthenticationToken(user.getEmail(), request.getPassword());
 
         String token = jwtTokenProvider.generateToken(
                 auth, user.getId(), user.getRole(), user.getEmail()
